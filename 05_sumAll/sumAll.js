e65d98223287e0a0d9f8574352f1f6fa5185f7a0
xonin-hush <xonindev@gmail.com>
2023-07-03 23:46:53 +0100

    solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let sum=0;
+    let temp=0;
+    if(typeof firstNum!="number")
+        return "ERROR";
+    if(typeof secondNum!="number")
+        return "ERROR";
+    if(firstNum<0||secondNum<0)
+        return "ERROR";
+    if(firstNum>secondNum)
+        {temp=firstNum;
+        firstNum=secondNum;
+        secondNum=temp;}
+    
+        for(let i=firstNum;i<=secondNum;i++)
+        {
+        
+            sum=sum+i;
+        }
+        return sum;
 
 };
 
