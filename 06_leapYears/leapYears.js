830805aee4155d8eaa6f06295bce72b6122bc9c3
xonin-hush <xonindev@gmail.com>
2023-07-04 00:11:31 +0100

    slove 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let answer=""
+    if(year%100==0&&year%400!=0)
+        return false;
+    if(year%4==0)
+        return true
+    if(year%4!=0)
+        return false
+
 
 };
 
