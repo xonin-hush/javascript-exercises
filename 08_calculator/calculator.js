22d9db349ea9f6fe5d9272a057df2529bfceb72a
xonin-hush <xonindev@gmail.com>
2023-07-25 13:35:55 +0100

    Solve 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b ) {
+  c=0
+	c=a+b
+  return c;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	c=0
+  c=a-b
+  return c;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+return array.reduce((total, currentValue)=> total+currentValue,0)
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+    return numbers.reduce((total, currentValue)=> total*currentValue,)
 };
 
-const power = function() {
-	
+const power = function(number, powerNumber) {
+  let number2=1
+	for (let i=0 ; i<powerNumber;i++){
+    number2=number2*number
+  }
+  return number2
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let fac=1
+	for (let i=1 ; i<=number ; i++)
+      fac=fac*i;
+      return fac;
 };
 
 // Do not edit below this line
