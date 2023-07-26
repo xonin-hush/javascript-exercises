375c637f5e57335514068c20af211ff966830a37
xonin-hush <xonindev@gmail.com>
2023-07-26 20:59:05 +0100

    Solve 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string)  {
+    let newStr = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return newStr.split("").reverse().join("") == newStr;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
