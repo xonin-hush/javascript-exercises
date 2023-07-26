5a587dc0e259699decdbcb04fb824aee666f66cb
xonin-hush <xonindev@gmail.com>
2023-07-26 23:14:18 +0100

    Solve 11-get the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(array) {
+newArray= array.map(getTitle)
+return newArray
+}
+function getTitle (element){
+    return element.title}
 
 // Do not edit below this line
 module.exports = getTheTitles;
